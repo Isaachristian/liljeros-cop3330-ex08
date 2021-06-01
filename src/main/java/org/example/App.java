2b7bca7f9d6acde70e0dd0d4136a4b53c07a421c
Isaac Liljeros <isaak.liljeros@gmail.com>
2021-06-01 19:59:04 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.util.Scanner;
+
+/**
+ * Pizza Party
+ */
+public class App {
+    public static void main( String[] args ) {
+        Scanner scanner = new Scanner(System.in);
+
+        double people, pizzas, slices;
+        int slicesPerPerson, slicesLeftover;
+
+        System.out.print("How many people? ");
+        people = scanner.nextInt();
+
+        System.out.print("How many pizzas do you have? ");
+        pizzas = scanner.nextInt();
+
+        slices = pizzas * 8;
+        slicesPerPerson = (int) (slices / people);
+        slicesLeftover  = (int) (slices - (slicesPerPerson * people));
+
+        System.out.println((int)people + " people with " + (int)pizzas + " pizzas");
+        System.out.println("Each person gets " + slicesPerPerson + " pieces of pizza.");
+        System.out.println("There are " + slicesLeftover + " leftover pieces");
+    }
+}
